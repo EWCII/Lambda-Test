674f96731f4d7a1e49744eb4179d8aed97573459
Carrington Eugene <eugenecarringtonii@icloud.com>
2018-05-18 18:25:01 -0500

    Still testing

---
+++ May 16, 2018 (Lambda Book)/BookRep.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace May_16__2018__Lambda_Book_
+{
+    class BookRep
+    {
+        public List<Book> GetBook()
+        {
+            return new List<Book>
+            {
+            new Book() { Title = "Title 1", Price = 5 },
+            new Book() { Title = "Title 2", Price = 7 },
+            new Book() { Title = "Title 3", Price = 14 },
+            new Book() { Title = "Title 4", Price = 23 },
+            new Book() { Title = "Title 5", Price = 100 },
+            };
+
+        }
+    }
+
+        class Book
+        {
+            public string Title { get; set; }
+            public int Price { get; set; }
+        }
+    
+}
---
+++ May 16, 2018 (Lambda Book)/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace May_16__2018__Lambda_Book_
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //use var here because this list has string and int
+            var books = new BookRep().GetBook();
+
+            //lambda
+            var cheapbooks = books.FindAll(book => book.Price < 10);
+
+            //2 line is method long way
+            //var cheapbooks = books.FindAll(cheapbook);
+
+            foreach (var book in cheapbooks)
+            {
+                Console.WriteLine(book.Title);
+            }
+            Console.ReadLine();
+        }
+        //public static bool cheapbook(Book book)
+        //{
+        //    return book.Price < 10;
+        //}
+    }
+}
+
